6f2ec258925816bf5acfa02ee4f3274e6b10a3fd
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2024-10-14 23:18:11 -0300

    criando modal para listagem de pedidos

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
 
-
 //modal de pedidos dos clientes
 let modal = document.querySelector('.pedidos')
 
 //botão que abre o modal quando clicado
 const clientPedidos = document.querySelector('.client-Pedidos')
-    clientPedidos.addEventListener('click', ()=>{
-        modal.style.display = 'block'
-    })
+clientPedidos.addEventListener('click', () => {
+  modal.style.display = 'block'
+})
 
 //botão que fecha o modal quando clicado
 const closeModal = document.getElementById('close-modal')
-    closeModal.addEventListener('click', () =>{
-        modal.style.display = 'none'
-    })
+closeModal.addEventListener('click', () => {
+  modal.style.display = 'none'
+})
+
